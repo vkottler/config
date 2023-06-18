9029c79dcaca53baf33bc1a768d322443f29c7c7
Vaughn Kottler <vaughnkottler@gmail.com>
2023-06-18 12:05:29 -0500

    Add initial yambs-native implementation

---
+++ python/vmklib/__init__.py
@@ -0,0 +1,31 @@
+"""
+A module exposing task registration interfaces for different kinds of projects.
+"""
+
+# built-in
+from pathlib import Path
+from typing import Dict
+
+# third-party
+from vcorelib.task.manager import TaskManager
+
+# internal
+from local.yambs import Yambs
+
+
+def register_yambs_native(
+    manager: TaskManager,
+    project: str,
+    cwd: Path,
+    substitutions: Dict[str, str],
+) -> bool:
+    """Register project tasks to the manager."""
+
+    deps = []
+
+    manager.register(Yambs("g", cwd), deps)
+    manager.register(Yambs("go", cwd, once=True), deps)
+    manager.register(Yambs("gb", cwd, once=True, build=True), deps)
+    manager.register(Yambs("gw", cwd, watch=True), deps)
+
+    return True
